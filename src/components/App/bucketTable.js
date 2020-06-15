63816ce486556b8b347c0655866db2bdbb59ca0b
Kristian Ringer <kristian.ringer@catalyst-au.net>
2020-06-15 19:31:55 +1000

    Fix some whitespace

--- src/components/App/bucketTable.js
+++ src/components/App/bucketTable.js
@@ -35,11 +35,11 @@ const BucketTable = ({data, updateData}) => {
 
     // Create an editable cell renderer
     const EditMultiline = ({
-                              value: initialValue,
-                              row: { values },
-                              column: { id },
-                              updateMyData, // This is a custom function that we supplied to our table instance
-                          }) => {
+                               value: initialValue,
+                               row: { values },
+                               column: { id },
+                               updateMyData, // This is a custom function that we supplied to our table instance
+                           }) => {
         // We need to keep and update the state of the cell normally
         const [value, setValue] = useState(initialValue)
         const onChange = e => {
@@ -119,56 +119,56 @@ const BucketTable = ({data, updateData}) => {
 
     return (
         <table {...getTableProps()} style={{ border: 'solid 1px black' }}>
-        <thead>
-        {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                    <th
-                        {...column.getHeaderProps()}
-                        style={{
-                            borderBottom: 'solid 3px red',
-                            background: 'aliceblue',
-                            color: 'black',
-                            fontWeight: 'bold',
-                        }}
-                    >
-                        {column.render('Header')}
-                    </th>
-                ))}
-            </tr>
-        ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-            prepareRow(row)
-            return (
-                <tr {...row.getRowProps()}>
-                    {row.cells.map(cell => {
-                        if (cell.column.id === 'month' || cell.column.id === 'remaining') {
+            <thead>
+            {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                        <th
+                            {...column.getHeaderProps()}
+                            style={{
+                                borderBottom: 'solid 3px red',
+                                background: 'aliceblue',
+                                color: 'black',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            {column.render('Header')}
+                        </th>
+                    ))}
+                </tr>
+            ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+                prepareRow(row)
+                return (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map(cell => {
+                            if (cell.column.id === 'month' || cell.column.id === 'remaining') {
+                                return (
+                                    <td {...cell.getCellProps()} className="bucketcell">
+                                        {cell.render('NonEditCell')}
+                                    </td>
+                                );
+                            }
+                            if (cell.column.id === 'description') {
+                                return (
+                                    <td {...cell.getCellProps()} className="bucketcell" style={{width: '400px'}}>
+                                        {cell.render('EditMultiline')}
+                                    </td>
+                                );
+                            }
                             return (
                                 <td {...cell.getCellProps()} className="bucketcell">
-                                    {cell.render('NonEditCell')}
+                                    {cell.render('Cell')}
                                 </td>
                             );
-                        }
-                        if (cell.column.id === 'description') {
-                            return (
-                                <td {...cell.getCellProps()} className="bucketcell" style={{width: '400px'}}>
-                                    {cell.render('EditMultiline')}
-                                </td>
-                            );
-                        }
-                        return (
-                            <td {...cell.getCellProps()} className="bucketcell">
-                                {cell.render('Cell')}
-                            </td>
-                        );
-                    })}
-                </tr>
-            )
-        })}
-        </tbody>
-    </table>
+                        })}
+                    </tr>
+                )
+            })}
+            </tbody>
+        </table>
     );
 }
 export default BucketTable;
