07be29fb1f0c4fdd895b9fb6fa7e1bbc937370d7
Kristian Ringer <kristian.ringer@catalyst-au.net>
2020-07-04 08:41:20 +1000

    Fix landing page when you're already signed in

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import * as ROLES from "../../constants/roles";
+import {AuthUserContext} from "../Session";
 const Landing = () => {
     return (
-        <div>
-            <h1>Sign up and sign in</h1>
-        </div>
+        <AuthUserContext.Consumer>
+            {authUser => {
+                if (authUser === null) {
+                    return <NonAuth />
+                }
+                if (authUser.roles[ROLES.ADMIN] || authUser.roles[ROLES.BASIC]) {
+                    return <Auth authUser={authUser} />
+                }
+                return <NonAuth />
+            }}
+        </AuthUserContext.Consumer>
     )
 };
 export default Landing;
+
+const NonAuth = () => {
+    return (
+        <div className="text-center mt-5">
+            <h1>Sign up or sign in!</h1>
+        </div>
+    );
+}
+
+const Auth = ({authUser}) => {
+    return (
+        <div className="text-center mt-5">
+            <h1>Hello <strong>{authUser.username}</strong></h1>
+            <h5>Use the links at the top to view client hours information.</h5>
+        </div>
+    );
+}
