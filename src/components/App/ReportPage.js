e094b923e206af6c80cf502858ad72d512f237c6
Kristian Ringer <kristian.ringer@catalyst-au.net>
2020-07-04 12:59:29 +1000

    Add filtering by user to report page

--- src/components/App/ReportPage.js
+++ src/components/App/ReportPage.js
@@ -1,13 +1,16 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { withAuthorization } from '../Session';
 import Sidebar from "../Navigation/sidebar";
 import {Container, Row, Col } from "react-bootstrap";
 import * as ROUTES from "../../constants/routes";
 import ReportBlankPage from "./ReportBlankPage"
 import SingleReportPage from "./SingleReportPage";
+import * as ROLES from "../../constants/roles";
 
 const ReportPage = ({firebase}) => {
     const [reset, setReset] = useState(false);
+    const [adminUsers, setAdminUsers] = useState([]);
+
     const clientIDinurl = window.location.pathname.split('report/').pop();
 
     let noClientSelected = false;
@@ -17,12 +20,27 @@ const ReportPage = ({firebase}) => {
     const resetPage = () => {
         setReset(!reset);
     }
+
+    useEffect(() => {
+        firebase.users().on('value', snapshot => {
+            const usersObject = snapshot.val();
+            const usersList = Object.keys(usersObject).map(key => ({
+                ...usersObject[key],
+                uid: key,
+            }));
+            const adminUsers = usersList.filter(user => {
+                return user.roles[ROLES.ADMIN] === ROLES.ADMIN;
+            });
+            setAdminUsers(adminUsers);
+        });
+    }, [firebase]);
+
     return (
         <div>
             <Container fluid>
                 <Row>
                     <Col xs={2} id="sidebar-wrapper">
-                        <Sidebar resetPage={resetPage}  />
+                        <Sidebar resetPage={resetPage} adminusers={adminUsers}  />
                     </Col>
                     <Col xs={10}>
                         {noClientSelected && <ReportBlankPage/>}
