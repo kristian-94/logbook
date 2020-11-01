388c82d0eca54e17f2881d97bc19751f9fcb9b65
Kristian Ringer <kristian.ringer@catalyst-au.net>
2020-11-01 18:04:15 +1100

    Fix up accounts page and display more user details there

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -5,9 +5,12 @@ const AccountPage = () => (
     <AuthUserContext.Consumer>
         {authUser => (
             <div>
-                <h1>Account: {authUser.email}</h1>
                 <div className="text-center">
+                    <h3 className="mt-3">Account: {authUser.email}</h3>
                     <hr/>
+                    <h3>Username: {authUser.username}</h3>
+                    <hr/>
+                    <h3>Role: {authUser.role}</h3>
                 </div>
             </div>
         )}
