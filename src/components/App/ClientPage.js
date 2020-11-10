39b6b55abf96408b0b80d8889217541e1d59acc4
Kristian Ringer <kristian.ringer@catalyst-au.net>
2020-11-11 08:46:38 +1100

    Add modal popup when you are about to edit a client that you are not the owner of

--- src/components/App/ClientPage.js
+++ src/components/App/ClientPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Sidebar from "../Navigation/sidebar";
 import {Container, Row, Col } from "react-bootstrap";
 import * as ROUTES from '../../constants/routes';
@@ -8,10 +8,16 @@ import ClientBlankPage from "./ClientBlankPage";
 import * as clientActions from "../../store/actions/Clients";
 import * as authActions from "../../store/actions/Auth";
 import {useDispatch, useSelector} from "react-redux";
+import SweetAlert from 'react-bootstrap-sweetalert';
+import {useHistory} from "react-router-dom";
 
 const ClientPage = () => {
+    const [confirmModal, setConfirmModal] = useState(null);
+    const [confirmed, setConfirmed] = useState(false);
     const dispatch = useDispatch();
     const currentUser = useSelector(state => state.auth.currentUser);
+    const activeClient = useSelector(state => state.clients.activeClient);
+    const history = useHistory();
     const clientIDinurl = window.location.pathname.split(ROUTES.CLIENTADMIN + '/').pop();
     let newClient = false;
     let noClient = false;
@@ -30,8 +36,44 @@ const ClientPage = () => {
         }
     }, [dispatch, currentUser]);
 
+    const onConfirmEditOtherOwnersClient = () => {
+        console.log('confirmed to edit this client, log this?');
+        setConfirmed(true);
+        setConfirmModal(null);
+    }
+
+    const goToReadOnlyPage = () => {
+        setConfirmModal(null);
+        history.push(ROUTES.CLIENTS + '/' + activeClient.id);
+    }
+
+    const modal = (
+        <SweetAlert
+            warning
+            showCancel
+            confirmBtnText="Client read only page"
+            confirmBtnBsStyle="warning"
+            cancelBtnText="Yes, I want to edit"
+            title="Are you sure?"
+            onCancel={() => onConfirmEditOtherOwnersClient()}
+            onConfirm={() => goToReadOnlyPage()}
+            focusCancelBtn={false}
+            focusConfirmBtn={false}
+        >
+            You are not the owner of this client. Do you want to edit the data anyway?
+        </SweetAlert>
+    );
+
+    // First check that we have a currentUser set and an activeclient.
+    if (noClient === false && confirmed === false && confirmModal === null && currentUser && Object.keys(activeClient).length !== 0) {
+        if (activeClient.ownerid !== currentUser.id) {
+            setConfirmModal(modal);
+        }
+    }
+
     return (
         <div>
+            {confirmModal}
             <Container fluid>
                 <Row>
                     <Col xs={2} id="sidebar-wrapper">
