7eba2eea4036671e26d715fbf435b400da65408b
Michael Palimaka <michaelpalimaka@catalyst-au.net>
2020-12-01 14:17:31 +1100

    provision: attempt to fix build

--- provision.sh
+++ provision.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-function build() {
-	composer install -vvv --no-dev
-	composer dump-autoload --optimize
-
-	npm install
-	npm run prod
-}
-
 cd /usr/local/lib/
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 composer self-update --1
 
+npm install
+
+
 pushd /var/www/site
 
-for dir in api client ; do
-	pushd ${dir}
-	build
-	chown -R www-data.www-data storage
-	chown -R www-data.www-data bootrap/cache
-	popd
-done
+pushd client
+npm run build
+popd
+
+pushd api
+composer install -vvv --no-dev
+composer dump-autoload --optimize
+chown -R www-data.www-data storage
+chown -R www-data.www-data bootsrap/cache
+popd
 
 rm -rf /root/.composer
