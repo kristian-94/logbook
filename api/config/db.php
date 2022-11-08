2645cc8dc42cd009699c0eb9081f933f876e19b6
Kristian <kristian.ringer@gmail.com>
2022-11-09 09:20:59 +1100

    Update env var injection and Dockerfile build for google cloud run

--- api/config/db.php
+++ api/config/db.php
@@ -1,13 +1,15 @@
 <?php
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../..");
-$dotenv->load();
+$host = getenv('DB_HOST');
+$database = getenv('DB_DATABASE');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => "pgsql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']}",
-    'username' => $_ENV['DB_USERNAME'],
-    'password' => $_ENV['DB_PASSWORD'],
+    'dsn' => "pgsql:host={$host};dbname={$database}",
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
