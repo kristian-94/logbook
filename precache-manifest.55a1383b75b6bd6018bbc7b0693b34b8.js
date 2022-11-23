a3c83d1558d95f91f6f1427a4738fd5da5f460b3
Kristian <kristian.ringer@gmail.com>
2022-11-23 09:23:52 +0000

    Deploy React app to GitHub Pages

---
+++ precache-manifest.55a1383b75b6bd6018bbc7b0693b34b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbb02df48d2b537998b8bdf5fdef756",
+    "url": "/logbook/index.html"
+  },
+  {
+    "revision": "1fcafce4a4193ce728a2",
+    "url": "/logbook/static/css/2.ca1594c8.chunk.css"
+  },
+  {
+    "revision": "1b03524fde8fb850ffd7",
+    "url": "/logbook/static/css/main.90d41d39.chunk.css"
+  },
+  {
+    "revision": "1fcafce4a4193ce728a2",
+    "url": "/logbook/static/js/2.2b350c05.chunk.js"
+  },
+  {
+    "revision": "d9e8a291be83e05279500325f496df54",
+    "url": "/logbook/static/js/2.2b350c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b03524fde8fb850ffd7",
+    "url": "/logbook/static/js/main.e293d5fb.chunk.js"
+  },
+  {
+    "revision": "46ef9dd9fa04d5c41b6c",
+    "url": "/logbook/static/js/runtime-main.425900b7.js"
+  }
+]);
